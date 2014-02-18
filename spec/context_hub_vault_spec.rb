1c4d76a2b8a5f8b157411c05f1e5aacdc08121c0
Robert Boone <robert@rlb3.com>
2014-02-18 10:26:42 -0600

    Spec for gem configuration

--- spec/context_hub_vault_spec.rb
+++ spec/context_hub_vault_spec.rb
@@ -5,7 +5,17 @@ describe ContextHubVault do
     ContextHubVault::VERSION.should_not be_nil
   end
 
-  it 'should do something useful' do
-    false.should eq(true)
+  it 'should be able to set its attributes via a configuration block' do
+    ContextHubVault.configure do |config|
+      config.host       = 'example.com'
+      config.auth_token = 'qwertyasdfqwerty'
+      config.version    = 42
+      config.app_id     = 50
+    end
+
+    expect(ContextHubVault.host).to       eq('example.com')
+    expect(ContextHubVault.app_id).to     eq(50)
+    expect(ContextHubVault.auth_token).to eq('qwertyasdfqwerty')
+    expect(ContextHubVault.version).to     eq(42)
   end
 end
