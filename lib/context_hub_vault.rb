f8e4af785036234d1e9fb3a1c0c7c5c898b8650b
Robert Boone <robert@rlb3.com>
2014-02-18 10:33:25 -0600

    Add configuration code

--- lib/context_hub_vault.rb
+++ lib/context_hub_vault.rb
@@ -1,5 +1,24 @@
-require "context_hub_vault/version"
+require 'context_hub_vault/version'
 
 module ContextHubVault
-  # Your code goes here...
+  class << self
+    attr_accessor :host, :version, :auth_token, :app_id
+
+    # config/initializers/context_hub_vault.rb (for instance)
+    #
+    # ContextHubVault.configure do |config|
+    #   config.host       = 'example.com'
+    #   config.auth_token = 'qwertyasdfqwerty'
+    #   config.version    = 42
+    #   config.app_id     = 50
+    # end
+    #
+    # elsewhere
+    #
+    # client = ContextHubVault::Client.new
+    def configure
+      yield self
+      true
+    end
+  end
 end
